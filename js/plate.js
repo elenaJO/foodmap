dee86f8b0ed8dbc70fa17bb93f04c606870a7fb3
elenaJO <ele.lucero1993@gmail.com>
2017-12-28 13:31:56 -0500

    añadiendo evento mouseover a las imagenes

--- js/plate.js
+++ js/plate.js
@@ -1,31 +1,49 @@
 $(document).ready(function() {
   $('#title-restaurant').text(localStorage.restaurant);
   var arrayNamePlate = [];
+  var arrayCodPlate = [];
   for (var i = 0; i <= 2; i++) {
     var codigoPlate = data[localStorage.distrito.toUpperCase()][localStorage.restaurant]['platos'][i]['codigo'];
     arrayNamePlate[i] = data[localStorage.distrito.toUpperCase()][localStorage.restaurant]['platos'][i]['nombre'];
+    arrayCodPlate[i] = codigoPlate;
     var appendPlate1 = '<div id="_cod_" class="_nombre_ images-plates"></div>';
     var appendReplace = appendPlate1.replace('_nombre_', codigoPlate).replace('_cod_', codigoPlate);
     $('#container-plates').append(appendReplace);
   }
+  // utilice mouseenter ya que solo quiero que solo quiero que se ejecute una vez para que no se repita el nombre del plato
+  function mouseenterImage(num) {
+    var cod = '#' + arrayCodPlate[num];
+    $(cod).addClass('red');
+    $(cod).append('<p class="name-plate">' + arrayNamePlate[num] + '</p>');
+  }
 
-  $('#container-plates > div:first').click(function(event) {
-    console.log(arrayNamePlate[0]);
+  function mouseleaveImage(num) {
+    var cod = '#' + arrayCodPlate[num];
+    $(cod).removeClass('red');
+    $('.name-plate').remove();
+  }
+
+  $('#container-plates > div:nth-child(2)').mouseenter(function(event) {
+    mouseenterImage(1);
   });
 
-  $('#container-plates > div:nth-child(2)').click(function(event) {
-    console.log(arrayNamePlate[1]);
+  $('#container-plates > div:nth-child(2)'). mouseleave(function() {
+    mouseleaveImage(1);
   });
 
-  $('#container-plates > div:nth-child(3)').click(function(event) {
-    console.log(arrayNamePlate[2]);
+  $('#container-plates > div:nth-child(3)').mouseenter(function(event) {
+    mouseenterImage(2);
+  });
+  
+  $('#container-plates > div:nth-child(3)'). mouseleave(function() {
+    mouseleaveImage(2);
+  });
+  
+  $('#container-plates > div:first').mouseenter(function() {
+    mouseenterImage(0);
   });
 
-  $('#container-plates > div:first').mouseover(function() {
-    console.log(arrayNamePlate[0]);
-  });
-
-  $('#container-plates > div:first').on('touchstart', function() {
-    console.log(arrayNamePlate[0]);
+  $('#container-plates > div:first'). mouseleave(function() {
+    mouseleaveImage(0);
   });
 });
