1ce0d72c06091c31d8da46307513978289ad43c2
elenaJO <ele.lucero1993@gmail.com>
2017-12-27 22:12:08 -0500

    añadiendo evento mouseover

--- js/plate.js
+++ js/plate.js
@@ -1,9 +1,31 @@
 $(document).ready(function() {
   $('#title-restaurant').text(localStorage.restaurant);
+  var arrayNamePlate = [];
   for (var i = 0; i <= 2; i++) {
     var codigoPlate = data[localStorage.distrito.toUpperCase()][localStorage.restaurant]['platos'][i]['codigo'];
+    arrayNamePlate[i] = data[localStorage.distrito.toUpperCase()][localStorage.restaurant]['platos'][i]['nombre'];
     var appendPlate1 = '<div id="_cod_" class="_nombre_ images-plates"></div>';
     var appendReplace = appendPlate1.replace('_nombre_', codigoPlate).replace('_cod_', codigoPlate);
     $('#container-plates').append(appendReplace);
   }
+
+  $('#container-plates > div:first').click(function(event) {
+    console.log(arrayNamePlate[0]);
+  });
+
+  $('#container-plates > div:nth-child(2)').click(function(event) {
+    console.log(arrayNamePlate[1]);
+  });
+
+  $('#container-plates > div:nth-child(3)').click(function(event) {
+    console.log(arrayNamePlate[2]);
+  });
+
+  $('#container-plates > div:first').mouseover(function() {
+    console.log(arrayNamePlate[0]);
+  });
+
+  $('#container-plates > div:first').on('touchstart', function() {
+    console.log(arrayNamePlate[0]);
+  });
 });
